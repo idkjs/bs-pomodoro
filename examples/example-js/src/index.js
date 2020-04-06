814906fc7de25c1e045b6298b377f73416491ac4
Alain Armand <aarmand.inbox@gmail.com>
2020-04-06 12:49:13 -0400

    example-js works

--- examples/example-js/src/index.js
+++ examples/example-js/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './App.css'
 import { ClockDemo} from './ClockDemo';
-console.log('Hello from tsx!')
+console.log('Hello from jsx!')
 
 ReactDOM.render(
   <ClockDemo initialTime="25:00"/>,
